c1aa1b6a4b6f20f86d20488349a5d184bff80118
jonathan <contact@grafikart.fr>
2022-10-31 18:59:07 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,19 @@
+import Reveal from "https://unpkg.com/reveal.js@4.4.0/dist/reveal.esm.js"
+import Highlight from "https://unpkg.com/reveal.js@4.4.0/plugin/highlight/highlight.esm.js"
+import Markdown from "https://unpkg.com/reveal.js@4.4.0/plugin/markdown/markdown.esm.js"
+import Notes from "https://unpkg.com/reveal.js@4.4.0/plugin/notes/notes.esm.js"
+
+let deck = new Reveal({
+   plugins: [Highlight, Markdown, Notes],
+});
+deck.initialize({
+  width: 1920,
+  height: 1080,
+  maxScale: 1,
+  progress: false,
+  controls: false,
+  hash: true,
+  // disableLayout: true,
+  transition: 'fade',
+  display: "flex",
+});
