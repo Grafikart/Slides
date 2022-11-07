cb5548420579797f08b09fdf16f4af3232b16f05
Grafikart <contact@grafikart.fr>
2022-11-07 13:30:39 +0100

    feat: navigation within iframe

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
-import Reveal from "https://unpkg.com/reveal.js@4.4.0/dist/reveal.esm.js"
-import Highlight from "https://unpkg.com/reveal.js@4.4.0/plugin/highlight/highlight.esm.js"
-import Markdown from "https://unpkg.com/reveal.js@4.4.0/plugin/markdown/markdown.esm.js"
-import Notes from "https://unpkg.com/reveal.js@4.4.0/plugin/notes/notes.esm.js"
+import Reveal from "https://unpkg.com/reveal.js@4.4.0/dist/reveal.esm.js";
+import Highlight from "https://unpkg.com/reveal.js@4.4.0/plugin/highlight/highlight.esm.js";
+import Markdown from "https://unpkg.com/reveal.js@4.4.0/plugin/markdown/markdown.esm.js";
+import Notes from "https://unpkg.com/reveal.js@4.4.0/plugin/notes/notes.esm.js";
 
 let deck = new Reveal({
-   plugins: [Highlight, Markdown, Notes],
+  plugins: [Highlight, Markdown, Notes],
 });
+const inIframe = document.referrer !== "";
 deck.initialize({
   width: 1920,
   height: 1080,
   maxScale: 1,
-  progress: false,
-  controls: false,
+  progress: inIframe ? true : false,
+  controls: inIframe ? true : false,
   hash: true,
   // disableLayout: true,
-  transition: document.body.dataset.transition ?? 'slide',
+  transition: document.body.dataset.transition ?? "slide",
   display: "flex",
 });
