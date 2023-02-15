eb4944ab4894ae676a9d97d2d7b503e52c6acb38
jonathan <contact@grafikart.fr>
2023-02-15 22:23:16 +0100

    fix: path for html

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,30 @@
-import { defineConfig } from 'vite'
-import {globby} from 'globby';
-import { fileURLToPath } from 'node:url'
+import { defineConfig } from "vite";
+import { globby } from "globby";
+import { fileURLToPath } from "node:url";
 
 const realPath = (path: string): string => {
-    return fileURLToPath(new URL(path, import.meta.url))
-}
+  return fileURLToPath(new URL(path, import.meta.url));
+};
 
 const pathToName = (path: string): string => {
-    return path.replaceAll('/', '_')
-        .replaceAll('src_','')
-        .replaceAll('.html', '')
-}
+  return path
+    .replaceAll("/", "_")
+    .replaceAll("src_", "")
+    .replaceAll(".html", "");
+};
 
-const paths = [
-  'index.html',
-  ...await globby(['src/**/index.html'])
-];
+const paths = ["index.html", ...(await globby(["src/**/index.html"]))];
 
 export default defineConfig({
-    build: {
-        rollupOptions: {
-          input: paths.reduce((acc, r) => {
-            return {
-                ...acc,
-                [pathToName(r)]: realPath(r)
-            }
-          }, {} as Record<string, string>),
-        },
-      },
-})
+  base: '/Slide/',
+  build: {
+    rollupOptions: {
+      input: paths.reduce((acc, r) => {
+        return {
+          ...acc,
+          [pathToName(r)]: realPath(r),
+        };
+      }, {} as Record<string, string>),
+    },
+  },
+});
